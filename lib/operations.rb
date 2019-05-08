f3a7f8c95d6284615644005a9c9b89e63a1e61d8
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-08 19:23:56 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,27 @@
 def unsafe?(speed)
-
+  result = FALSE
+  if speed > 60 || speed < 40
+    result = TRUE
+  end
+  return result
 end
 
 
 
 def not_safe?(speed)
-	
+  if speed > 60 
+    return TRUE
+  end
+  
+  if speed < 40
+    return TRUE
+  end
+  
+  if speed > 40 && speed < 60
+    return FALSE
+  end
+  
+  return true ? true : false
 end
 	
 
